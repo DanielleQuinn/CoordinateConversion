0fdaa660107a4a3eb4735c1d8a98ecfa095770cd
DanielleQuinn <danielle.quinn@mun.ca>
2022-11-03 15:53:20 -0300

    tidied DD_to_DDM function code

--- R/DD_to_DDM.R
+++ R/DD_to_DDM.R
@@ -1,40 +1,42 @@
 #' DD_to_DDM
 #'
-#' @param DD_input 
-#' @param axis 
+#' @param DD_input
+#' @param axis
 #'
 #' @return DDM
 #' @export
 #'
 #' @examples
-DD_to_DDM <-  function(DD_input, axis){
-  
+DD_to_DDM <-  function(DD_input, axis) {
   # Stop conditions
-  if(!is.numeric(DD_input)) stop("DD_input must be numeric")
-  if(length(DD_input) != 1) stop("DD_input must be of length 1")
-  if(!axis %in% c("horizontal", "vertical")) stop("axis must be one of horizontal or vertical")
+  if (!is.numeric(DD_input))
+    stop("DD_input must be numeric")
+  if (length(DD_input) != 1)
+    stop("DD_input must be of length 1")
+  if (!axis %in% c("horizontal", "vertical"))
+    stop("axis must be one of horizontal or vertical")
   
-  if(abs(DD_input) > 90 & axis == "vertical") {
+  if (abs(DD_input) > 90 & axis == "vertical") {
     warning("Vertical axis maximum is 90; NAs produced")
     return(NA)
   } else {
-    if(abs(DD_input) > 180 & axis == "horizontal") {
+    if (abs(DD_input) > 180 & axis == "horizontal") {
       warning("Horizontal axis maximum is 180; NAs produced")
       return(NA)
     } else {
-      return(paste0(trunc(absDeg),
-                    intToUtf8(176),
-                    " ",
-                    (abs(DD_input) - trunc(abs(DD_input))) * 60,
-                    " ",
-                    case_when(
-                      axis == "horizontal" & DD_input < 0 ~ "W",
-                      axis == "horizontal" & !DD_input < 0 ~ "E",
-                      axis == "vertical" & DD_input < 0 ~ "S",
-                      axis == "vertical" & !DD_input < 0 ~ "N"
-                      )
-                    )
-      )
+      return(paste0(
+        trunc(absDeg),
+        intToUtf8(176),
+        " ",
+        (abs(DD_input) - trunc(abs(DD_input))) * 60,
+        " ",
+        case_when(
+          axis == "horizontal" & DD_input < 0 ~ "W",
+          axis == "horizontal" & !DD_input < 0 ~ "E",
+          axis == "vertical" & DD_input < 0 ~ "S",
+          axis == "vertical" & !DD_input < 0 ~ "N"
+        )
+      ))
     }
   }
 }
