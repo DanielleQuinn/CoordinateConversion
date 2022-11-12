03f7c63aedbe30f8103ccbd7cf1081df0f46e783
DanielleQuinn <danielle.quinn@mun.ca>
2022-11-12 13:08:49 -0400

    adding stop conditions to UTM_to_DD()

--- R/UTM_to_DD.R
+++ R/UTM_to_DD.R
@@ -13,13 +13,17 @@
 #' UTM_to_DD(x = 624436.9, y = 7043729.8, zone = 59)
 #' 
 #' @export
-UTM_to_DD <-  function(x, y, zone, ellipsoid = "WGS84", return = "all") {
+UTM_to_DD <-  function(x, y, zone, hemisphere = "N", ellipsoid = "WGS84", return = "all") {
   
   # Stop Conditions
   if(!is.numeric(x)) stop("x argument must be numeric")
   if(!is.numeric(y)) stop("y argument must be numeric")
+  if(!between(x, 160000, 834000)) stop("x out of range")
+  if(!between(y, 0, 10000000)) stop("y out of range")
   if(!is.numeric(zone)) stop("zone argument must be numeric")
-  if(!return %in% c("lat", "lon", "all")) stop("Invalid return argument; please specify lat, lon, or all")
+  if(!between(zone, 0, 60)) stop("zone out of range")
+  if(!hemisphere %in% c("S", "N")) stop("Invalid hemisphere argument; should be one of S or N")
+  if(!return %in% c("lat", "lon", "all")) stop("Invalid return argument; should be one of lat, lon, or all")
   
   utm <- data.frame(lon = x, lat = y)
   coordinates(utm) <- ~lon+lat
